8088e24308317797967ccba9d265d809146034ce
Ravi RT Merugu <rrmerugu@gmail.com>
2018-04-21 19:13:38 +0530

    showing two widgets fixes

--- example/django_dashboard_bs4/views.py
+++ example/django_dashboard_bs4/views.py
@@ -5,12 +5,46 @@ import random
 
 logger = logging.getLogger(__name__)
 
+widgets = [
+    {
+        'name': "Weblinks Collection",
+        'API': "/api/indexing/solr/weblinks",
+        'fields': ['id', 'domain_s', 'created_dt'],
+        'filters': [{
+            'key': 'status_i',
+            'value': '404',
+            'display_name': "404 Status"
+        }, {
+            'key': 'domain_s',
+            'value': 'medium.com',
+            'display_name': "medium.com"
+        }, {
+            'key': 'domain_s',
+            'value': 'blog.github.com',
+            'display_name': "Github"
+        }],
+        'paginate_by': 6,
+        'filter_date_field': "created_dt"
+    },
+    {
+        'name': "WebFeeds Collection",
+        'API': "/api/indexing/solr/website_feeds",
+        'fields': ['id', 'title_s', 'domain_s', 'pub_date_dt'],
+        'filters': [],
+        'paginate_by': 6,
+        'filter_date_field': "pub_date_dt"
+    }
+]
+
 
 class DjangoDashboardBS4Base(TemplateView):
     template_name = "page.html"
-    widget_id = random.randint(312312, 1001231123)
 
     def get_context_data(self, **kwargs):
+        widgets_cleaned = []
+        for widget in widgets:
+            widget['widget_id'] = random.randint(312312, 1001231123)
+            widgets_cleaned.append(widget)
         return {
-            "widget_id": self.widget_id
+            "widgets": widgets_cleaned
         }
