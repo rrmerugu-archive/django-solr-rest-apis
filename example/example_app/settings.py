5852c9fd01b9e4e0f5d56c5ae4c4ae3f22049952
Ravi RT Merugu <rrmerugu@gmail.com>
2018-04-21 05:52:07 +0530

    module and example seperated

--- example/example_app/settings.py
+++ example/example_app/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
+sys.path.append('../')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
     'django_solr_rest_apis'
 ]
 
